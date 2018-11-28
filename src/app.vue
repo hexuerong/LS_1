02483422cfb31986892edd6b9adb6675709c48f8
hexuerong-7f <hexuerong@7fgame.com>
2018-11-28 14:14:35 +0800

    堆排序

--- src/app.vue
+++ src/app.vue
@@ -7,6 +7,8 @@
         <button @click="quickSort">快速排序</button>
         <button @click="insertSort(arr)">插入排序</button>
         <button @click="shellSort(arr)">希尔排序</button>
+        <button @click="heapSort(arr)">堆排序</button>
+        <button @click="heapSort1(arr)">假堆排序</button>
     </div>
 </template>
 
@@ -89,7 +91,72 @@ export default {
                 arr[j+1] = tmp;
             }
             console.log('插入排序结果：',arr);
-        }
+        },
+
+        heapSort1 (arr) {//假堆排序。(这种方式一趟大顶堆调整后，出现的并不是符合要求的大顶堆，只有根节点符合)
+            //16,7,3,20,17,8
+            for(let len = arr.length;len > 1;len --){
+                //这是一趟
+                for(let k = Math.floor(len / 2) - 1; k >= 0; k --){//从第一个非叶子节点开始，小于此下标的全是非叶子节点
+                    //k是当前非叶子节点的下标(第一个k节点的左节点肯定存在，右节点不一定存在)
+                    if( 2*k+2 < len ){//如果有右节点
+                        let t = arr[2*k+1] > arr[2*k+2] ? 2*k+1 : 2*k+2;//先找到左右子节点中的最大值
+                        if(arr[t] > arr[k]){
+                            //交换位置,把最大的放到当前非叶子节点
+                            let tmp = arr[t];
+                            arr[t] = arr[k];
+                            arr[k] = tmp;
+                        }
+                    }else{
+                        if(arr[2*k+1] > arr[k]){
+                            //交换位置,把最大的放到当前非叶子节点
+                            let tmp = arr[2*k+1];
+                            arr[2*k+1] = arr[k];
+                            arr[k] = tmp;
+                        }
+                    }
+                }
+                console.log('结束一趟大顶堆调整：',arr);
+                //已经找到最大的了，交换头尾
+                let tmp = arr[0];
+                arr[0] = arr[len-1];
+                arr[len-1] = tmp;
+            }
+        },
+
+        heapSort(arr){//堆排序。（这才是真正的）
+            const adjustHeap = function(arr,i,length){//调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+                let temp = arr[i];//先取出当前元素i
+                for(let k=i*2+1;k<length;k=k*2+1){//从i结点的左子结点开始，也就是2i+1处开始
+                    if(k+1<length && arr[k]<arr[k+1]){//如果左子结点小于右子结点，k指向右子结点
+                        k++;
+                    }
+                    if(arr[k] >temp){//如果子节点大于父节点，将子节点与父节点交换
+                        arr[i] = arr[k];
+                        arr[k] = temp;
+                        i = k;
+                    }else{//子节点都小于父节点，构造结束
+                        break;
+                    }
+                }
+            };
+
+            //1.构建大顶堆
+            for(let i=Math.floor(arr.length/2)-1;i>=0;i--){
+                //从第一个非叶子结点从下至上，从右至左调整结构
+                adjustHeap(arr,i,arr.length);
+            }
+            console.log('构造初始大顶堆：',arr);
+            //2.调整堆结构+交换堆顶元素与末尾元素
+            for(let j=arr.length-1;j>0;j--){
+                //将堆顶元素与末尾元素进行交换
+                let val = arr[0];
+                arr[0] = arr[j];
+                arr[j] = val;
+                adjustHeap(arr,0,j);//重新对堆进行调整，大顶堆构造好后的调整是从上至下，从左至右
+                console.log('调整后的大顶堆：',arr);
+            }
+        },
     }
 }
 </script>
